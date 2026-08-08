1a0617bc49154fcd9b5a02fb502d4f8edf68b943
SumitGo <sumitsharma3318@gmail.com>
2026-08-08 14:24:54 +0530

    modified revision file

--- 34_oops_revision.py
+++ 34_oops_revision.py
@@ -1,17 +1,54 @@
-class zoo:
-    prime = 'Elephant'
-    elegant = 'Humming Bird' 
-    __killer = 'Wolf'
-    def func(s,p, e, k):
-        s.prime = p
-        s.elegant = e
-        s.__killer = k
+# Revision of OOPS
+
+# class zoo:
+#     prime = 'Elephant'
+#     elegant = 'Humming Bird' 
+#     __killer = 'Wolf'
+#     def __init__(self, prime, elegant, killer):
+#         self.prime = prime
+#         self.elegant = elegant
+#         self.__killer = killer
+
+#     @staticmethod
+#     def newbie():
+#         return "m hu don"
+#     #def func(s,p, e, k):
+#         # s.prime = p
+#         # s.elegant = e
+#         # s.__killer = k
 
 
-ob = zoo()
-ob2 = zoo()
-ob.func('octopus', 'kankrej', 'Human')
-print(ob.prime, ob.elegant, ob._zoo__killer)
+# ob = zoo('octopus', 'kankrej', 'Human')
+# # ob2 = zoo()
+# # ob.func('octopus', 'kankrej', 'Human')
+# print(ob.prime, ob.elegant, ob._zoo__killer)
+# ob._zoo__killer = "ravan"
+# print(ob.prime, ob.elegant, ob._zoo__killer)
+# # ob.elegant = 'octopus'
+# # ob2.prime = 'Kankrej'
 
-ob.elegant = 'octopus'
-ob2.prime = 'Kankrej'
+class bank:
+    bname = 'dhandha bank'
+    bloc = 'khatra'
+    bifsc = 'dhnd0001234233'
+    def __init__(self, name, bal, accno):
+        self.name = name
+        self.bal = bal
+        self.accno = accno
+    def deposit(self, amt):
+        self.bal = self.add(self.bal, amt)
+
+    def withdrawl(self, amt):
+        if self.bal<amt:
+            return "Insufficient Balance"
+        self.bal = self.add(self.bal, amt)
+
+    @staticmethod
+    def add(a,b):
+        return a+b
+
+    @staticmethod
+    def sub(a,b):
+        return a-b
+
+    
